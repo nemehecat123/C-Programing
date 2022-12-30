0c03fd4f1d9406cbf13585afdb45270cb47bf75d
Mkavcic <matevz4ever@gmail.com>
2022-12-31 00:13:03 +0100

    isto nadstropje fixed

--- 1_task/main.c
+++ 1_task/main.c
@@ -40,34 +40,42 @@ void TakeInput(char nadstropja[][MAX_STRING_LENGHT], char *trenutnoNadstropje[])
     fgets(input, sizeof(input), stdin);
     char selectedIndex[MAX_STRING_LENGHT];
 
-    bool FalseInput = true;
-    
+    bool FalseInput = true; 
+    bool enakoNadstropje= false;//ce je vpisalo enako nadstropje
 
-    if (sscanf(input, "%s", &selectedIndex) == 1)
+    if (sscanf(input, "%s", &selectedIndex) == 1) // pogleda če ni kliknjen enter
     {
 
-
-
-        for (int i = 0; i < 9; i++)
-    {
-        printf("TEST INSIDE LOOP : %s\n",selectedIndex);
-        if (strcmp(nadstropja[i], selectedIndex) == 0)
+        for (int i = 0; i < 9; i++) // ta for loop pogleda samo če vnos ustreza kakšnemu od elementov
         {
-            FalseInput = false;
+            if (strcmp(nadstropja[i], selectedIndex) == 0) 
+            {
+                FalseInput = false; //če enmu ustreza pol pove da je kul in se nardi false
+            }
+            if (strcmp(*trenutnoNadstropje, selectedIndex) == 0) 
+            {
+                printf("to je enako nadstropje se enkraz upisano\n");
+                enakoNadstropje = true; //če enmu ustreza pol pove da je kul in se nardi false
+            }
         }
-    }
-    if (FalseInput == true) // če je vnos uporabnika napačen oz ni enak podanim možnostim bo prašalo še enkrat kaj je to
-    {
-        printf("Vneseno nadstropje ne obstaja\n UPORABA : premik dvigala [Number/P] | exit [enter]\n");
-        AvailableFloors(nadstropja, trenutnoNadstropje);
-        TakeInput(nadstropja, trenutnoNadstropje);
-    }
+        if (enakoNadstropje==true)
+        {
+             printf("vnesel si enako nadstropje v katerem se že\n UPORABA : premik dvigala [Number/P] | exit [enter]\n");
+            AvailableFloors(nadstropja, trenutnoNadstropje);
+            TakeInput(nadstropja, trenutnoNadstropje);
+        }
         
-        
+        if (FalseInput == true) // če je vnos uporabnika napačen oz ni enak podanim možnostim bo prašalo še enkrat kaj je to
+        {
+            printf("Vneseno nadstropje ne obstaja\n UPORABA : premik dvigala [Number/P] | exit [enter]\n");
+            AvailableFloors(nadstropja, trenutnoNadstropje);
+            TakeInput(nadstropja, trenutnoNadstropje);
+        }
+         // če pravilen vnos pol spremeni trenutno nadstropje da je vnečen vnos
         *trenutnoNadstropje = selectedIndex;
         printf("trenutno nadstropje je %s \n", *trenutnoNadstropje);
         AvailableFloors(nadstropja, trenutnoNadstropje);
-        TakeInput(nadstropja, trenutnoNadstropje);
+        TakeInput(nadstropja, trenutnoNadstropje); // in ponovi proces dokler ne stisne exita
     }
 }
 
@@ -75,7 +83,7 @@ void AvailableFloors(char nadstropja[][MAX_STRING_LENGHT], char *trenutnoNadstro
 {
     printf("Available floors : \n");
 
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 9; i++) // izpiše vsa možna nadstropja ki niso pouhna.
     {
         if (strcmp(nadstropja[i], *trenutnoNadstropje) != 0)
         {
